99721aec00c13d0ab6ebbf7ca2b225ec8b5bcc39
Nitesh Pandey <nitesh.pandey@kalgudi.com>
2023-07-02 20:05:47 +0530

    lecture on http calls

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,64 +1,37 @@
-import { Component } from '@angular/core';
-import { Product } from '../typings';
+import { Component, OnInit } from '@angular/core';
+import { BaseProduct, CartProduct } from '../typings';
+import { UserService } from '../service/user.service';
+import { TestService } from '../modules/test/services/test.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
-  productQuantity: number = 1;
-  productDetail: Product[] = [
-    {
-      image:
-        'https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/113fce8f-beb6-468d-a0c3-94020d6f09f5/image-optimization-cover-twist-opt.png',
-      name: 'Classroom of the Elite: Year 2',
-      price: 399,
-      details:
-        ' practical guide to help readers overcome negative thought patterns and live a more fulfilling life.',
-      category: 'Books',
-      quantity: this.productQuantity,
-    },
-    {
-      image:
-        'https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/113fce8f-beb6-468d-a0c3-94020d6f09f5/image-optimization-cover-twist-opt.png',
-      name: 'Harry Potter Spellbook:',
-      price: 199,
-      details:
-        ' practical guide to help readers overcome negative thought patterns and live a more fulfilling life.',
-      category: 'Books',
-      quantity: this.productQuantity,
-    },
-    {
-      image:
-        'https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/113fce8f-beb6-468d-a0c3-94020d6f09f5/image-optimization-cover-twist-opt.png',
-      name: 'Dont Believe',
-      price: 199,
-      details:
-        ' practical guide to help readers overcome negative thought patterns and live a more fulfilling life.',
-      category: 'Books',
-      quantity: this.productQuantity,
-    },
-    {
-      image:
-        'https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/113fce8f-beb6-468d-a0c3-94020d6f09f5/image-optimization-cover-twist-opt.png',
-      name: 'Gray Anatomy Outer Edition',
-      price: 499,
-      details:
-        ' practical guide to help readers overcome negative thought patterns and live a more fulfilling life.',
-      category: 'Books',
-      quantity: this.productQuantity,
-    },
-  ];
+export class ProductComponent implements OnInit {
+  public productDetails?: CartProduct[];
 
-  handleQuantity(val: string) {
-    if (this.productQuantity < 20 && val === 'plus') {
-      this.productQuantity = this.productQuantity + 1;
-    } else if (this.productQuantity > 1 && val === 'min') {
-      this.productQuantity = this.productQuantity - 1;
+  constructor(private testService: TestService) {}
+
+  ngOnInit(): void {
+    this.testService.getUsers().subscribe((res) => {
+      this.productDetails = res;
+    });
+  }
+
+  public handleQuantity(product: CartProduct, val: string): void {
+    if (
+      product.quantity &&
+      product.quantity < (product.inventoryCount || 0) - 1 &&
+      val === 'plus'
+    ) {
+      ++product.quantity;
+    } else if (product.quantity && product.quantity > 1 && val === 'min') {
+      --product.quantity;
     }
   }
 
-  addToCart(val: any) {
+  public addToCart(val: CartProduct): void {
+    val.quantity = 1;
     console.warn(val);
   }
 }
