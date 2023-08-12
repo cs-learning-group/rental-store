49a52e70f53b4d6b2bba48b108510850b03e3ec2
aaditya890 <mishraaaditya297@gmail.com>
2023-08-12 13:51:17 +0530

    seller-home &  headerChange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
-const routes: Routes = [
+const route: Routes = [
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/authentication/authentication.module').then(
-        (m) => m.AuthenticationModule
+        (r) => r.AuthenticationModule
       ),
+  },
+  {
+    path: 'search',
+    loadChildren: () =>
+      import('./modules/search/search.module').then((x) => x.SearchModule),
   },
   {
     path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'product',
+    component: ProductComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(route)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
