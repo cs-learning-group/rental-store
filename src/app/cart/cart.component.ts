3a528832e7f026c00593d94ee070212a4701785d
aaditya890 <mishraaaditya297@gmail.com>
2023-09-17 11:08:43 +0530

    cart-works

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,32 +6,7 @@ import { CartProduct } from '../typings';
   styleUrls: ['./cart.component.scss'],
 })
 export default class CartComponent implements OnInit {
-  cartItems: CartProduct[] = [
-    // {
-    //   image:
-    //     'https://m.media-amazon.com/images/I/41txBobui9S._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-    //   name: 'Every Day Hero',
-    //   price: 10,
-    //   quantity: 2,
-    //   id: 1,
-    // },
-    // {
-    //   image:
-    //     'https://m.media-amazon.com/images/I/41W-o6xu2bL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-    //   name: 'Do It Today',
-    //   price: 20,
-    //   quantity: 1,
-    //   id: 2,
-    // },
-    // {
-    //   image:
-    //     'https://m.media-amazon.com/images/I/61MeD6FXDTL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-    //   name: 'Hindu Rashtra',
-    //   price: 15,
-    //   quantity: 3,
-    //   id: 3,
-    // },
-  ];
+  cartItems: CartProduct[] = [];
   ngOnInit(): void {
     let value: any = localStorage.getItem('cart');
     value = JSON.parse(value);
